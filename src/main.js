5eddf147538c8b2c7f544888817a5467c12c709d
Amruta <amritakulkarni04@gmail.com>
2024-01-04 21:34:46 +0000

    Expense calculator added with composition api

---
+++ src/main.js
@@ -0,0 +1,11 @@
+
+import { createApp } from 'vue';
+import Toast from "vue-toastification";
+// Import the CSS or use your own!
+import "vue-toastification/dist/index.css";
+import './assets/style.css';
+import App from './App.vue'
+
+const app = createApp(App);
+app.use(Toast);
+app.mount('#app');
